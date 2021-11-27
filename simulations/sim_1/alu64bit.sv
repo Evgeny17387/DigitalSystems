8d1363f8a6aa211cc0d16a86f9a95beee5c51177
Evgeny Ostrovsky <evostrov@microsoft.com>
2021-11-27 15:57:22 +0200

    complete all implementations

--- simulations/sim_1/alu64bit.sv
+++ simulations/sim_1/alu64bit.sv
@@ -7,7 +7,34 @@ module alu64bit (
     output logic cout        // Carry out
 );
 
+	logic [62:0] connector;
+	
+	genvar i;
+	generate
+		
+		for (i = 0; i <= 63; i++) begin
+		
+			if (i == 0) begin
 
+				alu1bit alu1bit_inst(.a(a[i]), .b(b[i]), .cin(cin), .op(op), .s(s[i]), .cout(connector[i]));
+
+			end
+
+			else if (i == 63) begin
+
+				alu1bit alu1bit_inst(.a(a[i]), .b(b[i]), .cin(connector[i-1]), .op(op), .s(s[i]), .cout(cout));
+
+			end
+			
+			else begin
+
+				alu1bit alu1bit_inst(.a(a[i]), .b(b[i]), .cin(connector[i-1]), .op(op), .s(s[i]), .cout(connector[i]));
+
+			end
+		
+		end
+	
+	endgenerate
 
 
 endmodule
--- simulations/sim_1/alu64bit_test.sv
+++ simulations/sim_1/alu64bit_test.sv
@@ -1,9 +1,28 @@
 module alu64bit_test;
 
+	parameter tpd = 10;
 
+    logic [63:0] a;
+    logic [63:0] b;
+    logic cin;
+    logic [1:0] op;
 
+    logic [63:0] s;
+    logic cout;
 
+    alu64bit alu64bit_dut(.a(a), .b(b), .cin(cin), .op(op), .s(s), .cout(cout));
 
+    initial begin
 
+        cin = 1'b1;
+	
+		// NOR
+		
+        op = 2'b10;
+
+        a = 64'hffffffffffffffff;
+        b = 64'b0;
+		
+    end
 
 endmodule
